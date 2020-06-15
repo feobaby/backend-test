61831895a7913d00c8370807346343e8cc2313be
Funmilayo Olaiya <olufunmilayo335@gmail.com>
2020-06-15 15:52:40 +0100

    feat(devEnv): set up dev environment

---
+++ src/database/migrations/create-1-user.js
@@ -0,0 +1,35 @@
+export default {
+  up(queryInterface, Sequelize) {
+    return queryInterface.sequelize.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";').then(() => queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.DataTypes.UUID,
+        defaultValue: Sequelize.literal('uuid_generate_v4()'),
+      },
+      fullname: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    }));
+  },
+  down: (queryInterface) => queryInterface.dropTable('Users'),
+};
---
+++ src/database/models/user.mode.js
@@ -0,0 +1,18 @@
+export default (sequelize, DataTypes) => {
+  const User = sequelize.define('Users', {
+    fullname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+  return User;
+};
